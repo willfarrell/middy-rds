45fbe3a459ffdb422b8998f0ae7102c2cb8bb25e
will Farrell <will.farrell@gmail.com>
2021-06-27 09:39:55 -0600

    fix: clean up

--- pg.js
+++ pg.js
@@ -22,12 +22,11 @@ const rdsMiddleware = (opts = {}) => {
 
     options.config = Object.assign({}, defaultConnection, options.config, values)
 
-    let pool = new options.client(options.config)
+    const pool = new options.client(options.config)
 
-    let db
     try {
-      db = await pool.connect()
-
+      // cache the connection, not the credentials as they may change over time
+      return  pool.connect()
     } catch(e) {
       // Connection failed for some reason
       // log and clear cache, force re-connect
@@ -35,23 +34,15 @@ const rdsMiddleware = (opts = {}) => {
       clearCache([options.cacheKey])
       throw new Error(e.message) //createError(500, e.message)
     }
-
-    // const query = async (sql, params) => {
-    //   return db.query(escape(sql, params))
-    // }
-
-    // cache the connection, not the credentials as they may change over time
-    return db
   }
 
   const rdsMiddlewareBefore = async (request) => {
     const {value}  = processCache(options, fetch, request)
-
     Object.assign(request.context, { db: await value }) // await due to fetch being a promise
   }
   const rdsMiddlewareAfter = async (request) => {
     if (options.cacheExpiry === 0) {
-      request.context.db.end()
+      await request.context.db.end()
     }
   }
   const rdsMiddlewareOnError = rdsMiddlewareAfter
