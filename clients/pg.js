812f76d1164860db8f6732f743ad12d1fa99f933
will Farrell <willfarrell@proton.me>
2023-05-02 13:36:19 -0600

    feat: force connection per request
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- clients/pg.js
+++ clients/pg.js
@@ -5,12 +5,13 @@ import ssl from '../lib/ssl.js'
 
 const defaults = {
   client: undefined,
-  config: {},
+  config: {
+    application_name: process.env.AWS_LAMBDA_FUNCTION_NAME
+  },
   internalData: undefined,
-  contextKey: 'sql',
+  contextKey: 'sql', // TODO change to rds
   disablePrefetch: false,
   cacheKey: 'rds',
-  cachePasswordKey: 'rds',
   cacheExpiry: -1
 }
 
@@ -27,19 +28,20 @@ const rdsMiddleware = (opts = {}) => {
       options.config.port = Number.parseInt(process.env.PGPORT ?? 5432)
     }
     if (!options.config.password) {
+      console.time('iamToken')
       options.config.password = await iamToken(options.config)
+      console.timeEnd('iamToken')
     }
 
     const Client = options.client
     const pool = new Client(options.config)
 
     // cache the connection, not the credentials as they change over time
-    return pool
+    const forceConnection = await pool
       .connect()
       .catch((e) => {
         // Connection failed for some reason
         // log and clear cache, force re-connect
-        clearCache([options.cachePasswordKey])
         clearCache([options.cacheKey])
         prefetch = undefined
         throw new Error(e.message) // createError(500, e.message)
@@ -47,6 +49,8 @@ const rdsMiddleware = (opts = {}) => {
       .finally(() => {
         options.config.password = undefined
       })
+    forceConnection.release()
+    return pool
   }
 
   let prefetch
@@ -56,12 +60,22 @@ const rdsMiddleware = (opts = {}) => {
 
   const rdsMiddlewareBefore = async (request) => {
     const { value } = prefetch ?? processCache(options, fetch, request)
-    Object.assign(request.context, { [options.contextKey]: await value }) // await due to fetch being a promise
+    const pool = await value // await due to fetch being a promise
+    Object.assign(request.context, {
+      [options.contextKey]: await pool.connect(), // for serial single connection
+      [options.contextKey + 'Pool']: pool // for parallel connections
+    })
+
+    console.log(pool)
   }
   const rdsMiddlewareAfter = async (request) => {
-    if (options.cacheExpiry === 0) {
-      await request.context[options.contextKey].end()
-    }
+    // try/catch for if an error is thrown after this ran the first time
+    try {
+      request.context[options.contextKey].release()
+      if (options.cacheExpiry === 0) {
+        await request.context[options.contextKey + 'Pool'].end()
+      }
+    } catch (e) {}
   }
   const rdsMiddlewareOnError = rdsMiddlewareAfter
 
